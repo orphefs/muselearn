79cff58be53cda162e744f17caac7ab0dfc967fe
Orfeas Kypris <kypriso@gmail.com>
2017-08-03 23:36:04 +0100

    [master] Add ROOT_DIR variable in definitions.py

--- src/scraping/create_youtube_job.py
+++ src/scraping/create_youtube_job.py
@@ -1,9 +1,11 @@
+from muselearn.definitions import ROOT_DIR
+import os
 from apiclient.discovery import build  # pip install google-api-python-client
 from apiclient.errors import HttpError  # pip install google-api-python-client
 from oauth2client.tools import argparser  # pip install oauth2client
 import pandas as pd  # pip install pandas
 
-with open('config/_developer.key') as infile:
+with open(os.path.join(ROOT_DIR, 'config', '_developer.key')) as infile:
     developer_key = infile.readline()
 
 DEVELOPER_KEY = developer_key
@@ -17,7 +19,6 @@ options = args
 
 youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
 
-
 # Call the search.list method to retrieve results matching the specified
 # query term.
 search_response = youtube.search().list(
@@ -29,15 +30,13 @@ search_response = youtube.search().list(
 ).execute()
 videos = {}
 
-
 # Add each result to the appropriate list, and then display the lists of
 # matching videos.
 # Filter out channels, and playlists.
 for search_result in search_response.get("items", []):
     if search_result["id"]["kind"] == "youtube#video":
-    # videos.append("%s" % (search_result["id"]["videoId"]))
+        # videos.append("%s" % (search_result["id"]["videoId"]))
         videos[search_result["id"]["videoId"]] = search_result["snippet"]["title"]
-
 
 # print "Videos:\n", "\n".join(videos), "\n"
 s = ','.join(videos.keys())
@@ -51,5 +50,7 @@ for i in videos_list_response['items']:
     temp_res = dict(v_id=i['id'], v_title=videos[i['id']])
     temp_res.update(i['statistics'])
     res.append(temp_res)
-print(pd.DataFrame.from_dict(res))
 
+df = pd.DataFrame.from_dict(res)
+result = df.sort_values(by=df['viewCount'])
+print(result)

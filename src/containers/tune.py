240400068fed82de2142d59ac7663d5b13a70e58
Orfeas Kypris <kypriso@gmail.com>
2017-08-06 02:32:16 +0100

    [master] Clean up

--- src/containers/tune.py
+++ src/containers/tune.py
@@ -1,6 +1,3 @@
-import json
-import numpy as np
-from _collections_abc import abstractmethod
 
 
 class _Metadata(object):
@@ -27,76 +24,20 @@ class AudioMetadata(_Metadata):
         self.bpm = bpm
 
 
-class Segment(object):
-    def __init__(self, floor_entry_time,
-                 floor_exit_time,
-                 delta_floor,
-                 entry_structure_id,
-                 exit_structure_id,
-                 delta_pressure_of_transition,
-                 checks=None):
-        self._floor_entry_time = floor_entry_time
-        self._floor_exit_time = floor_exit_time
-        self._delta_floor = delta_floor
-        self._entry_structure_id = entry_structure_id
-        self._exit_structure_id = exit_structure_id
-        self._delta_pressure_of_transition = delta_pressure_of_transition
-
-        self._duration = floor_exit_time - floor_entry_time
-        self._checks = checks
+class Snippet(object):
+    def __init__(self):
+        raise NotImplementedError
 
     def __repr__(self):
-        return 'Segment(floor_entry_time=%r, ' \
-               'floor_exit_time=%r, ' \
-               'delta_floor=%r, ' \
-               'entry_structure_id=%r,  ' \
-               'exit_structure_id=%r, ' \
-               'delta_pressure_of_transition=%r) \n' % (self._floor_entry_time,
-                                                        self._floor_exit_time,
-                                                        self._delta_floor,
-                                                        self._entry_structure_id,
-                                                        self._exit_structure_id,
-                                                        self._delta_pressure_of_transition)
+        return 'vlah=%r' % (self._vlah
+                                                )
 
     def __str__(self):
-        return "{},{},{},{},{},{}".format(self._floor_entry_time,
-                                          self._floor_exit_time,
-                                          self._delta_floor,
-                                          self._entry_structure_id,
-                                          self._exit_structure_id,
-                                          self._delta_pressure_of_transition)
+        return "{}".format(self._vlah)
 
     @classmethod
     def from_data_frame_row(cls, df):
         raise NotImplementedError
-
-    @property
-    def checks(self):
-        return self._checks
-
-    @checks.setter
-    def checks(self, checklist):
-        self._checks = checklist
-
-    @property
-    def duration(self):
-        return self._duration
-
-    @property
-    def delta_pressure_of_transition(self):
-        return self._delta_pressure_of_transition
-
-    @delta_pressure_of_transition.setter
-    def delta_pressure_of_transition(self, value):
-        self._delta_pressure_of_transition = value
-
-    @property
-    def delta_floor(self):
-        return self._delta_floor
-
-    @delta_floor.setter
-    def delta_floor(self, value):
-        self._delta_floor = value
 
 
 class Audio(object):
@@ -121,46 +62,22 @@ class Audio(object):
 
     @classmethod
     def from_file(cls, path_to_audio_file):
-        """Factory method to create trajectory from floorchange and building"""
+        """Factory method"""
         file = load_audio_file()
         audio = Audio(path_to_file=path_to_audio_file, metadata=None)
         return audio
 
-    def write_to_file(self, path_to_floorchange):
-
-        with open(path_to_floorchange, 'w') as outfile:
-            outfile.write('floor_entry_time, '
-                          'floor_exit_time, '
-                          'delta_floor, '
-                          'entry_structure_id, '
-                          'exit_structure_id, '
-                          'delta_pressure_of_transition \n')
-            for segment in self._segments:
-                outfile.write(str(segment))
-                outfile.write('\n')
-
-    @property
-    def segments(self):
-        return self._segments
-
     def __iter__(self):
-        for segment in self._segments:
-            yield segment
+        for snippet in self._snippets:
+            yield snippet
 
     def __getitem__(self, index):
-        return self._segments[index]
+        return self._snippets[index]
 
     def remove(self, segment):
-        if segment in self._segments:
-            self._segments.remove(segment)
+        if snippet in self._snippets:
+            self._snippets.remove(snippet)
 
 
 if __name__ == '__main__':
-    segment1 = Segment(0, 1, 1, 100, 101, -0.7, [])
-    segment2 = Segment(1, 2, -1, 101, 100, 0.8, [])
-
-    trajectory = Song([segment1, segment2])
-    for segment in trajectory:
-        print(segment)
-
-    print(trajectory[0])
+    pass
--- src/converters/mp3_converter.py
+++ src/converters/mp3_converter.py
@@ -1,4 +1,5 @@
 import abc
+import re
 from pathlib import Path
 import magic
 import subprocess
@@ -14,9 +15,11 @@ class Converter(object):
 
     # Create based on class name:
     @staticmethod
-    def factory(type, path_to_input_file):
+    def from_type(type, path_to_input_file):
         if type == "wav_to_mp3":
             return _Wav2Mp3(path_to_input_file)
+        elif type == "mp3_to_wav":
+            return _Mp32Wav(path_to_input_file)
         else:
             raise TypeError
 
@@ -27,17 +30,43 @@ class Converter(object):
 
 
 class _Wav2Mp3(Converter):
-
     def __init__(self, path_to_input_file):
         super(_Wav2Mp3, self).__init__(path_to_input_file)
-        print(magic.from_file(str(path_to_input_file)))
-        self._path_to_output_file = Path(self._path_to_input_file.stem + '.mp3')
+        mime = magic.from_file(str(path_to_input_file), mime=True)
+        if not re.search('wav', mime):
+            raise TypeError("Invalid input file binary format.")
+        self._path_to_output_file = Path(self._path_to_input_file.with_suffix('.mp3'))
 
     def convert(self):
         cmd = "lame --preset insane {infile} {outfile}".format(infile=str(self._path_to_input_file),
-                                               outfile=str(self._path_to_output_file))
+                                                               outfile=str(self._path_to_output_file))
         subprocess.call(cmd, shell=True)
+        return self._path_to_output_file
+
+
+class _Mp32Wav(Converter):
+    def __init__(self, path_to_input_file):
+        super(_Mp32Wav, self).__init__(path_to_input_file)
+        mime = magic.from_file(str(path_to_input_file), mime=True)
+        if not re.search('mpeg', mime):
+            raise TypeError("Invalid input file binary format.")
+        self._path_to_output_file = Path(self._path_to_input_file.with_suffix('.wav'))
+
+    def convert(self):
+        cmd = "lame --decode {infile} {outfile}".format(infile=str(self._path_to_input_file),
+                                                        outfile=str(self._path_to_output_file))
+        subprocess.call(cmd, shell=True)
+        return self._path_to_output_file
 
 
 if __name__ == '__main__':
-    Converter.factory('wav_to_mp3', Path('/home/orphefs/Documents/Code/muselearn/muselearn/data/music/candyman.wav')).convert()
+    # path_to_fake_wav = '/home/orphefs/Documents/Code/muselearn/muselearn/data/music/fake.wav'
+    # path_real_wav = '/home/orphefs/Documents/Code/muselearn/muselearn/data/music/_candyman.wav'
+    # path_to_mp3 = Converter.from_type('wav_to_mp3',
+    #                     Path(path_real_wav)).convert()
+
+    path_to_mp3 = '/home/orphefs/Documents/Code/muselearn/muselearn/data/music/candyman.mp3'
+    path_to_wav = Converter.from_type('mp3_to_wav',
+                                      Path(path_to_mp3)).convert()
+
+    print(path_to_mp3)

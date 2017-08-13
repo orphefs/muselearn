bf2704d77cf7b2435a4f2ffe092e899798031e29
Orfeas Kypris <kypriso@gmail.com>
2017-08-13 14:32:52 +0100

    [master] Put fixtures into collection

--- src/signal_processing/test_main.py
+++ src/signal_processing/test_main.py
@@ -11,24 +11,31 @@ def init_dc_signal(request):
 
 
 @pytest.fixture(scope='class')
-def init_ac_signal(request):
+def init_ac_signal_triangle(request):
     request.cls.waveform = np.array([-1, 1] * 5)
     request.cls.sampling_rate = 0.5
 
+@pytest.fixture(scope='class')
+def init_ac_signal_square(request):
+    request.cls.waveform = np.array([-1, 1] * 5)
+    request.cls.sampling_rate = 0.5
 
-@pytest.mark.usefixtures('init_dc_signal', 'init_ac_signal')
+fixture_collection = ('init_dc_signal', 'init_ac_signal_triangle', 'init_ac_signal_square')
+
+
+@pytest.mark.usefixtures(fixture_collection)
 class TestEnergyComputation(unittest.TestCase):
     def test_compute_energy(self):
         assert compute_energy(waveform=self.waveform, sampling_rate=self.sampling_rate) == 20.0
 
 
-@pytest.mark.usefixtures('init_dc_signal', 'init_ac_signal')
+@pytest.mark.usefixtures(fixture_collection)
 class TestPowerComputation(unittest.TestCase):
     def test_compute_power(self):
         assert compute_power(waveform=self.waveform, sampling_rate=self.sampling_rate) == 1.0
 
 
-@pytest.mark.usefixtures('init_dc_signal', 'init_ac_signal')
+@pytest.mark.usefixtures(fixture_collection)
 class TestDurationCalculation(unittest.TestCase):
     def test_duration(self):
         assert duration(waveform=self.waveform, sampling_rate=self.sampling_rate) == 20.0

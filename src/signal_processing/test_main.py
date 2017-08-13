779e694fdf442ad9d66df7710a90522ccfdf381c
Orfeas Kypris <kypriso@gmail.com>
2017-08-13 14:02:27 +0100

    [master] Add AC signal test

--- src/signal_processing/test_main.py
+++ src/signal_processing/test_main.py
@@ -7,17 +7,23 @@ from muselearn.src.signal_processing.main import compute_energy
 @pytest.fixture(scope='class')
 def init_dc_signal(request):
     request.cls.waveform = np.ones(10)
-    request.cls.sampling_rate = 1
+    request.cls.sampling_rate = 0.5
 
 
-@pytest.mark.usefixtures('init_dc_signal')
+@pytest.fixture(scope='class')
+def init_ac_signal(request):
+    request.cls.waveform = np.array([-1, 1] * 5)
+    request.cls.sampling_rate = 0.5
+
+
+@pytest.mark.usefixtures('init_dc_signal', 'init_ac_signal')
 class TestEnergyComputation(unittest.TestCase):
     def test_compute_energy(self):
-        assert compute_energy(waveform=self.waveform, sampling_rate=self.sampling_rate) == 10.0
+        assert compute_energy(waveform=self.waveform, sampling_rate=self.sampling_rate) == 20.0
 
 
 if __name__ == '__main__':
     import os
     import subprocess
 
-    subprocess.call(['pytest',  os.path.basename(__file__)])
+    subprocess.call(['pytest', os.path.basename(__file__)])

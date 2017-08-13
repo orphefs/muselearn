80c98b6ca0efabe8a476acd663ca618ca5ea6b01
Orfeas Kypris <kypriso@gmail.com>
2017-08-13 14:58:07 +0100

    [master] add function annotations and signal normalization function

--- src/signal_processing/main.py
+++ src/signal_processing/main.py
@@ -5,16 +5,23 @@ def main():
     print(compute_energy(np.ones(10), 1))
 
 
-def compute_energy(waveform, sampling_rate):
-    return np.sum(amplitude ** 2 for amplitude in waveform) * (1 / sampling_rate)
+def compute_energy(waveform: np.array[np.int64], sampling_rate: int) -> np.float64:
+    return np.float64(np.sum(amplitude ** 2 for amplitude in waveform) * (1 / float(sampling_rate)))
 
 
-def compute_power(waveform, sampling_rate):
+def compute_power(waveform: np.array[np.int64], sampling_rate: int) -> np.float64:
     return compute_energy(waveform, sampling_rate) / duration(waveform, sampling_rate)
 
 
-def duration(waveform, sampling_rate):
-    return len(waveform) * (1/sampling_rate)
+def duration(waveform: np.array[np.int64], sampling_rate: int)-> np.float64:
+    return np.float64(np.float64(len(waveform)) * (1 / float(sampling_rate)))
+
+
+def normalize(waveform: np.array[np.int64], method: str) -> np.array[np.int64]:
+    if method == 'peak':
+        return waveform / np.max(waveform)
+    elif method == 'rms':
+        raise NotImplementedError
 
 
 

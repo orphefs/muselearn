d47eedfb9f0f9a4e6b3c4ef6da348e6b75ee2058
Orfeas Kypris <kypriso@gmail.com>
2017-09-23 15:25:06 +0100

    [master] Implement main function

--- src/main.py
+++ src/main.py
@@ -1,10 +1,31 @@
-import pytest
+from muselearn.src.signal_processing.advanced import compute_energy_over_time_window
+from muselearn.src.scraping.mp3_downloader import download_mp3_from_youtube_link
+from muselearn.src.converters.type_converter import TypeConverter
+from muselearn.src.converters.file_converter import FileConverter
+from pathlib import Path
 
-@pytest.fixture
-def smtp():
-    import smtplib
-    return smtplib.SMTP("smtp.gmail.com", 587, timeout=5)
 
-def test_ehlo(smtp):
-    response, msg = smtp.ehlo()
-    assert response == 250
+def main():
+    waveform = collect_data()
+    print(len(waveform), type(waveform))
+
+
+def collect_data():
+    path_to_mp3 = download_mp3_from_youtube_link('https://www.youtube.com/watch?v=IS6n2Hx9Ykk')
+    path_to_wav = FileConverter.from_type('mp3_to_wav',
+                                          Path(path_to_mp3)).convert()
+    waveform = TypeConverter.from_type('wav_to_numpy', path_to_wav)
+    return waveform
+
+
+def process_data(waveform):
+    compute_energy_over_time_window(waveform)
+    pass
+
+
+def plot_data():
+    pass
+
+
+if '__name__' == '__main__':
+    main()
